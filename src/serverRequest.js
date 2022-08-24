2d8a3a2817d5adee8a44f8c0d589aa70fd1b36f0
Artyom Orsik <artsiom.orsik@gmail.com>
2022-08-24 16:44:07 +0300

    fixed percentage 2.0

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -3,39 +3,36 @@ document.addEventListener("DOMContentLoaded", () => {
   const infoTextArea = document.getElementById("info");
   const sendButton = document.getElementById("send");
   const openButton = document.getElementById("open");
+  let hasOriginalText = false;
+  let wordsCounter = false;
 
   const regExp = /[^(\d+(.\d+)?)]/g;
 
   const EMPTY_STRING = "";
   const MIN_WORDS_COUNT = 3;
   const MAX_PERCENTAGE = 100;
-  const CLASS_BTN_DISABLED = "btn_disabled";
-  const SEND_BTN_TITLE = `Enter at least ${MIN_WORDS_COUNT} words!`;
+  const SEND_BTN_TITLE = `Enter at least ${MIN_WORDS_COUNT} words and download original text!`;
 
   infoTextArea.disabled = true;
-  sendButton.classList.add(CLASS_BTN_DISABLED);
+  sendButton.disabled = true;
   sendButton.title = SEND_BTN_TITLE;
 
   checkTextArea.addEventListener("keyup", (e) => {
     const wordsArray = e.target.value
       .split(" ")
       .filter((str) => str.length > 0);
-    const wordsCounter = wordsArray.length;
-
-    if (wordsCounter >= MIN_WORDS_COUNT) {
+    wordsCounter = wordsArray.length >= MIN_WORDS_COUNT;
+    if (wordsCounter && hasOriginalText) {
       sendButton.disabled = false;
-      sendButton.classList.remove(CLASS_BTN_DISABLED);
       sendButton.title = EMPTY_STRING;
 
       if (e.code === "Enter") {
         sendButton.disabled = false;
-        sendButton.classList.remove(CLASS_BTN_DISABLED);
         sendButton.title = EMPTY_STRING;
         sendButton.click();
       }
     } else {
       sendButton.disabled = true;
-      sendButton.classList.add(CLASS_BTN_DISABLED);
       sendButton.title = SEND_BTN_TITLE;
     }
   });
@@ -54,8 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
       );
       const text = await response.text();
 
-      const answerNumber =
-        MAX_PERCENTAGE - parseInt(text.replace(regExp, EMPTY_STRING));
+      const answerNumber = MAX_PERCENTAGE - parseInt(text.replace(regExp, EMPTY_STRING));
       document.querySelector("#opacity").classList.add("opacity");
       document.querySelector("#result").innerHTML = `${answerNumber}% unique`;
     } catch (e) {
@@ -71,10 +67,16 @@ document.addEventListener("DOMContentLoaded", () => {
         `${window.location.origin}/cgi-bin/text.cgi`
       );
       const text = await response.text();
+      hasOriginalText = true;
+      
       infoTextArea.innerHTML = text;
-      infoTextArea.style.color = "black";
       document.querySelector("#opacity").classList.remove("opacity");
       document.querySelector("#result").innerHTML = "";
+
+      if (wordsCounter && hasOriginalText) {
+        sendButton.disabled = false;
+        sendButton.title = EMPTY_STRING;
+      }  
     } catch (e) {
       throw new Error(e.message);
     }
